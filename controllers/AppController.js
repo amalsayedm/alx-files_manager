5a47f0a3d7961eb7c9aab77627229a961b8a73e5
amalsayedm <amal.sayedm2@gmail.com>
2024-03-09 08:02:11 +0200

    import-linking

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-named-as-default */
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
+
 export default class AppController {
   static getStatus(req, res) {
     res.status(200).json({

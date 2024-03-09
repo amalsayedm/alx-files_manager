b9d7db69ecef62cec73673f2df14b4cece0a9363
amalsayedm <amal.sayedm2@gmail.com>
2024-03-09 07:01:45 +0200

    usercontroller

---  routes/index.js
+++  routes/index.js
@@ -2,6 +2,7 @@
 import { Express } from 'express';
 import AppController from '../controllers/AppController';
 import { APIError, errorResponse } from '../middlewares/error';
+import UsersController from '../controllers/UsersController';
 
 /**
  * Injects routes with their handlers
@@ -10,6 +11,7 @@ import { APIError, errorResponse } from '../middlewares/error';
 const injectRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
+  api.post('/users', UsersController.postNew);
 
   api.all('*', (req, res, next) => {
     errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`), req, res, next);
